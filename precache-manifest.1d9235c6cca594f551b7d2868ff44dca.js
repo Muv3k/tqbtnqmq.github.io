bb5de2f90aa20610368f380c54ca9d1ed745ca6e
Muv3k <67328941+Muv3k@users.noreply.github.com>
2020-07-28 07:35:40 +0300

    Add files via upload

---
+++ precache-manifest.1d9235c6cca594f551b7d2868ff44dca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ee5f0e0e8b368294812338e05a187c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceeea53aba4ee64604b5",
+    "url": "/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "ec2bcf6d240c538ba24b",
+    "url": "/static/css/main.cfd95f68.chunk.css"
+  },
+  {
+    "revision": "ceeea53aba4ee64604b5",
+    "url": "/static/js/2.dd45b58d.chunk.js"
+  },
+  {
+    "revision": "f5d0e843a7dc80dc1dd1e4f52f636121",
+    "url": "/static/js/2.dd45b58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2bcf6d240c538ba24b",
+    "url": "/static/js/main.57eebaff.chunk.js"
+  },
+  {
+    "revision": "00cb06f39158b4017381",
+    "url": "/static/js/runtime-main.c1e3e2e2.js"
+  },
+  {
+    "revision": "fce1a6ff39d553d6ea43b1db8c4c0fd1",
+    "url": "/static/media/balance.fce1a6ff.png"
+  },
+  {
+    "revision": "48dc45685bb350c81fbcf6668124374b",
+    "url": "/static/media/dollar.48dc4568.png"
+  }
+]);
